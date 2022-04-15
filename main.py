0271ef549302c5c27dac54fe9f9196a2b21a6ade
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-04-15 20:35:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+from data import data
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+  
+
+@app.route("/planet")
+def planet():
+    name = request.args.get("name")
+    planet_data = next(item for item in data if item["name"] == name)
+    
+
+if __name__ == "__main__":
+    app.run()
