4e33348732f4ca8a6fc6b60768d31fe8dc69ae4b
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-10-08 17:17:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ from data import data
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-  
 
-@app.route("/planet")
-def planet():
-    name = request.args.get("name")
-    planet_data = next(item for item in data if item["name"] == name)
-    
+
 
 if __name__ == "__main__":
     app.run()
